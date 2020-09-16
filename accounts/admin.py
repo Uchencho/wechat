26bd718ebc8b08893dbbba6f9dd2353d686033ef
Uchencho <aloziekelechi17@gmail.com>
2020-09-16 11:52:40 +0100

    started the chat app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+class MyUserAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name',
+                                         'phone_number','house_add')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    list_display = [
+                    'id',
+                    'email',
+                    'phone_number',
+                    'is_active',
+                    'is_superuser']
+
+admin.site.register(User, MyUserAdmin)
