05043bc8699c08e5c988df4b4ba5be1acd07f43a
Uchencho <aloziekelechi17@gmail.com>
2020-10-16 14:05:11 +0100

    cors heders wahala

--- wechat/settings.py
+++ wechat/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'channels',
 
     'accounts',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'wechat.urls'
 ASGI_APPLICATION = 'wechat.routing.application'
+CHANNEL_LAYERS = {
+    'default' : {
+        'BACKEND' : 'channels_redis.core.RedisChannelLayer',
+        'CONFIG' : {
+            "hosts" : [('127.0.0.1', 6379)]
+        },
+    },
+}
 
 TEMPLATES = [
     {
@@ -141,9 +151,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 
